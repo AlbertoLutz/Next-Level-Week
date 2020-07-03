fc1e7450b05daa8b50aadba48a2d9cf92e8b98ea
Alberto Lutz <albertolutzdias@gmail.com>
2020-07-02 22:34:30 -0300

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,12 @@ const modal = document.querySelector(".modal")
 const close = document.querySelector(".modal .header a")
 
 
-buttonSearch.addEventListener("click", () => {
+window.onload function() {
+    buttonSearch.addEventListener("click", () => {
     modal.classList.remove("hide")
 })
 
 close.addEventListener("click", () => {
     modal.classList.add("hide")
 })
+}
